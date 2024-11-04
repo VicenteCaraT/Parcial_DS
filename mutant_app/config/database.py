3bcd7e06be99668f039f5b318acf21b7381f60c7
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-11-04 11:19:13 -0300

    adding volume for persistancdb persistence, fixing db inicialization

--- mutant_app/config/database.py
+++ mutant_app/config/database.py
@@ -1,13 +1,16 @@
 import os
+import time
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.exc import OperationalError
 from models.dna_model import Base
 
+# Cargar las variables de entorno
 env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../.env'))
 load_dotenv(env_path)
 
+# Configuración de conexión a MySQL
 MYSQL_HOST = os.getenv('MYSQL_HOST')
 MYSQL_PORT = os.getenv('MYSQL_PORT')
 MYSQL_DB = os.getenv('MYSQL_DB')
@@ -16,21 +19,40 @@ MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 
 # URI de conexión a la base de datos
 DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
-# DATABASE_URI = 'mysql+pymysql://root:vichi123@db:3306/mutante'
 
 
-class Database: 
+def check_mysql_service():
+    """Verifica si el servicio MySQL está activo."""
+    temp_engine = create_engine(DATABASE_URI)
+    try:
+        with temp_engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return True
+    except OperationalError:
+        return False
+    finally:
+        temp_engine.dispose()
+
+
+class Database:
     _instance = None
-    
+
     def __init__(self):
-        self._create_database_if_not_exists()
         print("Connecting TO: ", DATABASE_URI)
         
-        self.engine = create_engine(DATABASE_URI)
-        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-
-        if self.check_connection():
-            self.create_tables()
+        # Intentar conectar a la base de datos, con reintentos en caso de fallo
+        for attempt in range(5):  # Intentar 5 veces
+            try:
+                self.engine = create_engine(DATABASE_URI)
+                self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+                if self.check_connection():
+                    print("Database connected.")
+                break  
+            except OperationalError as e:
+                print(f"Attempt {attempt + 1}: Error connecting to the database: {e}")
+                time.sleep(5)  # Esperar 5 segundos antes de volver a intentar
+        else:
+            raise Exception("Unable to connect to the database after multiple attempts.")
 
     def __new__(cls):
         if not cls._instance:
@@ -63,6 +85,7 @@ class Database:
             del self._session
 
     def check_connection(self):
+        """Verifica si la conexión con la base de datos es posible."""
         try:
             with self.engine.connect() as connection:
                 connection.execute(text("SELECT 1"))
@@ -71,16 +94,3 @@ class Database:
         except OperationalError as e:
             print(f"Error connecting to database: {e}")
             return False
-
-    def _create_database_if_not_exists(self):
-        temp_database_uri = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/'
-        temp_engine = create_engine(temp_database_uri)
-        
-        try:
-            with temp_engine.connect() as conn:
-                conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {MYSQL_DB}"))
-            print(f"Database '{MYSQL_DB}' checked/created successfully.")
-        except Exception as e:
-            print(f"Error creating database '{MYSQL_DB}': {e}")
-        finally:
-            temp_engine.dispose()
--- mutant_app/main.py
+++ mutant_app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi import HTTPException
-from controllers.mutant_controller import router as mutant_router
-from config.database import Database
 from sqlalchemy.exc import OperationalError
 import time
+
+from controllers.mutant_controller import router as mutant_router
+from config.database import Database, check_mysql_service
+
 app = FastAPI()
-db = Database()
 
 # Manejo de excepciones personalizado
 class InstanceNotFoundError(Exception):
@@ -23,17 +23,22 @@ async def instance_not_found_exception_handler(request, exc):
 app.include_router(mutant_router, prefix='/mutant')
 
 # Inicializa la base de datos y crea tablas al iniciar la app
-
 @app.on_event("startup")
 async def startup_event():
-    for _ in range(5):  # Intentar 5 veces
-        try:
-            db.create_tables()  # Intentar crear tablas
-            break  # Si tiene éxito, salir del bucle
-        except OperationalError:
-            time.sleep(5)  # Esperar 5 segundos antes de volver a intentar
-
-
+    print("Waiting for MySQL service to become available...")
+    for _ in range(10):  # Intentar hasta 10 veces
+        if check_mysql_service():
+            print("MySQL service is active. Initializing Database...")
+            db = Database()  # Inicializar la base de datos solo cuando MySQL esté disponible
+            db.create_tables()  # Crear las tablas
+            break
+        else:
+            print("MySQL not available yet, retrying in 5 seconds...")
+            time.sleep(5)
+    else:
+        print("MySQL service was not available after multiple attempts.")
+        raise Exception("No se pudo conectar a la base de datos después de múltiples intentos.")
+        
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
